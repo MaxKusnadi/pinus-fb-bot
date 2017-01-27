9632b2b86e002e46b4ccbf1b828379cc28e7864f
max <max.kusnadi@gmail.com>
2017-01-27 23:12:18 +0800

    asdxa

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,4 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-# if __name__ == '__main__':
-#     app.run()
+from app import views
