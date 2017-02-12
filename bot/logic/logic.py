d47b18d0c6186a0bf2a7d92d158dc7e4699b798a
max <max.kusnadi@gmail.com>
2017-02-12 16:14:21 +0800

    finished logic

--- bot/logic/logic.py
+++ bot/logic/logic.py
@@ -50,18 +50,107 @@ class Logic(object):
     def parse_message(self, user, messaging_event):
         if "quick_reply" in messaging_event["message"].keys():
             self.process_quick_reply(user, messaging_event["message"]["quick_reply"]["payload"])
-        try:
-            message_text = messaging_event["message"]["text"]  # the message's text
-        except KeyError:
-            self.send_message_text(user.fb_id, "Thanks for the likes, {name}!".format(name=user.first_name))
         else:
-            if message_text.lower() == "order":
-                self.start_order(user.fb_id)
+            try:
+                message_text = messaging_event["message"]["text"]  # the message's text
+            except KeyError:
+                self.send_message_text(user.fb_id, "Thanks for the likes, {name}!".format(name=user.first_name))
             else:
-                self.send_message_text(user.fb_id, "Hi, {name}! You can order flower by typing 'order'! ".format(name=user.first_name))
+                if message_text.lower() == "order":
+                    self.start_order(user.fb_id)
+                else:
+                    self.send_message_text(user.fb_id, "Hi, {name}! You can order flower by typing 'order'! ".format(name=user.first_name))
 
     def process_quick_reply(self, sender_id, payload):
-        pass
+        if payload == "Packet A" or payload == "Packet B":
+            self.process_quantity(sender_id, payload)
+        elif payload in ["1", "2", "3"]:
+            self.confirm_order(sender_id, payload)
+        elif payload == "No":
+            self.send_message_text(sender_id, "You can always order flower again by typing 'order'! ")
+        else:
+            self.store_order(sender_id, payload)
+
+    def store_order(self, sender_id, payload):
+        order = self.order.create_order(sender_id)
+        order = self.order.update_order_status_by_order_id(order.id, "CONFIRMED")
+        orderItem = self.item.create_order_item(order.id, payload['description'], payload['quantity'])
+        user = self.user.get_user_by_fb_id(sender_id)
+        self.send_message_text(sender_id, "Thanks for ordering {name}! Your order has been confirmed! :)".format(user.first_name))
+
+    def confirm_order(self, sender_id, payload):
+        logging.info("starting order for {recipient}".format(recipient=recipient_id))
+
+        data = json.dumps({
+            "recipient": {
+                "id": recipient_id
+            },
+            "message": {
+                "text": "Which flower do you want?",
+                "quick_replies":[
+                    {
+                        "content_type":"text",
+                        "title":"Yes",
+                        "payload": payload,
+                        "image_url":"https://cdn.pixabay.com/photo/2013/06/23/19/47/rose-140853_960_720.jpg"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"No",
+                        "payload":"No",
+                        "image_url":"https://cdn.pixabay.com/photo/2013/05/26/12/14/rose-113735_960_720.jpg"
+                    }
+                ]
+            }
+        })
+        r = requests.post(self.LINK, params=self.PARAMS, headers=self.HEADERS, data=data)
+        if r.status_code != 200:
+            logging.info(r.status_code)
+            logging.info(r.text)
+
+
+
+    def process_quantity(self, sender_id, payload):
+        logging.info("processing quantity for {recipient}".format(recipient=recipient_id))
+
+        data = json.dumps({
+            "recipient": {
+                "id": recipient_id
+            },
+            "message": {
+                "text": "How many {} do you want?".format(payload),
+                "quick_replies":[
+                    {
+                        "content_type":"text",
+                        "title":"1",
+                        "payload": {
+                            "description": payload,
+                            "quantity": 1
+                            }
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"2",
+                        "payload": {
+                            "description": payload,
+                            "quantity": 2
+                            }
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"3",
+                        "payload": {
+                            "description": payload,
+                            "quantity": 3
+                            }
+                    },
+                ]
+            }
+        })
+        r = requests.post(self.LINK, params=self.PARAMS, headers=self.HEADERS, data=data)
+        if r.status_code != 200:
+            logging.info(r.status_code)
+            logging.info(r.text)
 
     def start_order(self, recipient_id):
         logging.info("starting order for {recipient}".format(recipient=recipient_id))
