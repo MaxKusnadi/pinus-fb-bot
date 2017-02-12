04f79c0fd63b24b948d1c11a94c9324875af62aa
max <max.kusnadi@gmail.com>
2017-02-12 13:53:26 +0800

    change view

--- bot/views/index.py
+++ bot/views/index.py
@@ -38,9 +38,11 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+                    if "quick_reply" in messaging_event["message"].keys():
+                    	send_message(sender_id, messaging_event["message"]["quick_reply"]["payload"])
+                    else:
+	                    send_message(sender_id, message_text)
 
-                    send_message(sender_id, message_text)
-                    send_message(sender_id, messaging_event["message"]["quick_reply"]["payload"])
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
