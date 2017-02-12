23cd7e6ecc33cafbd94aa129b3a140332c286b3b
max <max.kusnadi@gmail.com>
2017-02-12 13:36:34 +0800

    change view

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from flask import Flask
-from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
@@ -11,7 +10,6 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-api = Api(app)
 
 import bot.views.index
 
